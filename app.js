8fc7d8c6b6339e445d9d3a57b3365118a2213243
Niccolò Cacace <niccolo99cacace@gmail.com>
2023-05-31 18:10:04 +0200

    +apertura zalando +ricerca "scarpe nike" +click su bottone per accettare i cookie +click sul bottone "Ordina" per aprire il menù +click sul bottone "offerte" per filtrare solo i prodotti in offerta

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const path = require('path');
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const path = require("path");
 
-
-(async function example() {
-    let driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeService(new chrome.ServiceBuilder('C:\\Users\\napol\\Documents\\BrowserDriversSelenium\\chromeDriver\\chromedriver.exe'))
+(async () => {
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeService(
+      new chrome.ServiceBuilder(
+        "C:\\Users\\napol\\Documents\\BrowserDriversSelenium\\chromeDriver\\chromedriver.exe"
+      )
+    )
     .build();
   try {
-    await driver.get('http://www.google.com');
+    await driver.get("https://www.zalando.it/");
+    await driver.manage().window().maximize();
+    const searchBox = await driver.findElement(By.name("q")); // 'q' è il nome generico per il campo di ricerca.
+    await searchBox.sendKeys("scarpe nike"); //ciò che scrivo nel campo di ricerca
+    await searchBox.sendKeys(Key.RETURN); //simulo "Enter"
 
-  } catch(err) {
-    console.log("Errore prelevato dal catch",err);
+    //premo il bottone per accettare i cookie 
+    const Cookiebutton = await driver.wait(until.elementLocated(By.xpath("//button[@id='uc-btn-accept-banner']")), 10000);
+    Cookiebutton.click();
+    
+  //premo il bottone "Ordina" per poi far mostrare solo i prodotti in offerta
+    const Ordinabutton = await driver.wait(until.elementLocated(By.xpath("//body/div[4]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]")), 10000);
+    Ordinabutton.click();
+
+    //premo il bottone "offerte" per far mostrare solo i prodotti in offerta
+    const Offertebutton = await driver.wait(until.elementLocated(By.xpath("//body/div[16]/div[1]/div[2]/div[1]/form[1]/div[1]/div[5]/div[1]/label[1]")), 10000);
+    Offertebutton.click();
+
+  
+    
+  } catch (err) {
+    console.log("Errore prelevato dal catch", err);
   }
 })();
