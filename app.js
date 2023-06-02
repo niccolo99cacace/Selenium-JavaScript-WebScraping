e3e11823bce01db54a5dda8b44d5e2efdabd6984
Niccolò Cacace <niccolo99cacace@gmail.com>
2023-06-02 02:40:00 +0200

    +preleva dalla prima pagina il NOME - PREZZO - BRAND dei prodotti solo della pima pagina Raccogliendoli in un array ordinato

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ const path = require("path");
     await driver.get("https://www.zalando.it/");
     await driver.manage().window().maximize();
     const searchBox = await driver.findElement(By.name("q")); // 'q' è il nome generico per il campo di ricerca.
-    await searchBox.sendKeys("scarpe nike"); //ciò che scrivo nel campo di ricerca
+    await searchBox.sendKeys("scarpe da calcio"); //ciò che scrivo nel campo di ricerca
     await searchBox.sendKeys(Key.RETURN); //simulo "Enter"
 
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
     //premo il bottone per accettare i cookie 
-    const Cookiebutton = await driver.wait(until.elementLocated(By.xpath("//button[@id='uc-btn-accept-banner']")), 10000);
+    const Cookiebutton = await driver.wait(until.elementLocated(By.xpath("//button[@id='uc-btn-deny-banner']")), 20000);
     Cookiebutton.click();
     
   //premo il bottone "Ordina" per poi far mostrare solo i prodotti in offerta
@@ -27,10 +29,54 @@ const path = require("path");
     Ordinabutton.click();
 
     //premo il bottone "offerte" per far mostrare solo i prodotti in offerta
-    const Offertebutton = await driver.wait(until.elementLocated(By.xpath("//body/div[16]/div[1]/div[2]/div[1]/form[1]/div[1]/div[5]/div[1]/label[1]")), 10000);
+    const Offertebutton = await driver.wait(until.elementLocated(By.css("body.is-uc:nth-child(2) div._0ofwUT.KVKCn3.DpImHu.CIcWFg.DcVjqF.MMd_43._0xLoFW._78xIQ-.LyRfpJ.zvYeCP div.VAkC61.hj1pfK._8n7CyI.KLaowZ div.KLaowZ form:nth-child(1) div.B_F_kS._0xLoFW._aLdvK.parent._78xIQ- div.ywxJ27.JT3_zV._0xLoFW._78xIQ-.EJ4MLB:nth-child(5) div.OxR88i._0xLoFW.JT3_zV.FCIprz.LyRfpJ > label._0Foye_.KxHAYs._2kjxJ6.FxZV-M.LkH0Rh.Z1Xqqm._8xiD-i.JT3_zV.ZkIJC-.Md_Vex.JCuRr_.CzGCn5._0xLoFW.FCIprz._4F506m.oHZ46w.a2XEVb")), 10000);
     Offertebutton.click();
 
-  
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+/*
+    const product = await driver.wait(until.elementLocated(By.xpath("//body/div[4]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[2]/div[2]/div[2]/div")), 10000);
+    let html = await product.getAttribute('innerHTML');
+    console.log(html);   */
+
+
+    const products = await driver.wait(until.elementsLocated(By.css("._5qdMrS.w8MdNG.cYylcv.BaerYO._75qWlu.iOzucJ.JT3_zV._Qe9k6")), 10000);
+
+    let productData = [];
+
+    let x = 0;
+
+    for (let product of products) {
+
+      
+      let name = await product.findElements(By.css('.KxHAYs.lystZ1.FxZV-M._4F506m.ZkIJC-.r9BRio.qXofat.EKabf7.nBq1-s._2MyPg2'));
+
+      
+      if(name.length === 0)  console.log("NULLA");
+
+      else {   
+
+        let price = await product.findElements(By.css('.KxHAYs.lystZ1.dgII7d._3SrjVh'));
+        let brand = await product.findElements(By.css('._6zR8Lt.lystZ1.FxZV-M._4F506m.ZkIJC-.r9BRio.qXofat.EKabf7.nBq1-s._2MyPg2')); 
+
+
+        let productName = await name[0].getText();
+        let productPrice = await price[0].getText();
+        let productBrand = await brand[0].getText();
+        productData.push({productName,productPrice,productBrand}); 
+        x++;
+      }
+
+      // Aggiungi le informazioni del prodotto all'array
+      //productData.push({name, price});
+
+
+      
+    }
+    console.log(productData);
+    console.log(x);
+    
+
     
   } catch (err) {
     console.log("Errore prelevato dal catch", err);
